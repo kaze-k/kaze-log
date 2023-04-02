ce80e10cd925295c050c9c76fcbca8eb0e2957a7
kaze-k <kerkaze@outlook.com>
2023-04-03 01:14:41 +0800

    更新: 2023-04-03 01:14:37

--- js/runtime/runtime.js
+++ js/runtime/runtime.js
@@ -1,4 +1,4 @@
-const nol=function(s){return 9<s?s:"0"+s};function createTime(){var s=Math.round(new Date("03/01/2023 00:00:00").getTime()/1e3);let a=Math.round((new Date).getTime()/1e3)-s;s=new Array(0,0,0,0,0);0===s[0]&&(s[0]="0"+s[0]),31536e3<=a&&(s[0]=a/31536e3,a%=31536e3),86400<=a&&(s[1]=Math.floor(a/86400),a%=86400),3600<=a&&(s[2]=nol(Math.floor(a/3600)),a%=3600),60<=a&&(s[3]=nol(Math.floor(a/60)),a%=60),0<=a&&(s[4]=nol(a));let e="";e=Number(s[2])<20&&6<Number(s[2])?`
+const nol=s=>9<Number(s)?s:"0"+s;function createTime(){var s=Math.round(new Date("03/01/2023 00:00:00").getTime()/1e3);let a=Math.round((new Date).getTime()/1e3)-s;s=new Array("000","00","00","00","00");31536e3<=a&&(s[0]=a/31536e3,a%=31536e3),86400<=a&&(s[1]=Math.floor(a/86400),a%=86400),3600<=a&&(s[2]=nol(Math.floor(a/3600)),a%=3600),60<=a&&(s[3]=nol(Math.floor(a/60)),a%=60),0<=a&&(s[4]=nol(a));let e="";e=Number(s[2])<19&&6<Number(s[2])?`
       <img class='boardsign' src='https://img.shields.io/badge/klog-白天好-6adea8?style=social&logo=Ghostery&logoColor=343a43' title='X_X'>
       <div class='textTip'>
         本站已加载了
