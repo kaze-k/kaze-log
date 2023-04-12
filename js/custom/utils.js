cc024e8e8305c3c555e463217876ab37311a5666
kaze-k <kerkaze@outlook.com>
2023-04-13 01:33:22 +0800

    更新: 2023-04-13 01:33:18

--- js/custom/utils.js
+++ js/custom/utils.js
@@ -1,10 +1,15 @@
-document.addEventListener('scroll', function () {
-  var go_down = document.querySelector('#go-down')
-  var isbottom = Math.abs(document.documentElement.scrollHeight - window.pageYOffset - document.documentElement.clientHeight) <= 1
+document.addEventListener('DOMContentLoaded', function () {
+  const hide_down = () => {
+    var go_down = document.querySelector('#go-down')
+    var isbottom = Math.abs(document.documentElement.scrollHeight - window.pageYOffset - document.documentElement.clientHeight) <= 1
 
-  if (isbottom) {
-    go_down.style.display = "none"
-  } else {
-    go_down.style.display = "block"
+    if (isbottom) {
+      go_down.style.display = "none"
+    } else {
+      go_down.style.display = "block"
+    }
   }
+
+  window.addEventListener('scroll', hide_down)
+  window.addEventListener('click', hide_down)
 })
